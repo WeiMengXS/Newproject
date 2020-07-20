989264b6e7c7c14060de1cd30c3803f6b6981ecf
WeiMengXS <1254688559@qq.com>
2020-07-20 17:08:28 +0800

    vector_lic

---
+++ 6-17/6-17/test.c
@@ -0,0 +1,22 @@
+#include<iostream>
+//#include<string.h>
+using namespace std;
+class A
+{
+public:
+	int Getva()//const
+	{
+		vv = 1;
+		return vv;
+	}
+private:
+	 int vv;
+};
+//int main()
+//{
+//	char s[] = "is nome";
+//	char t[] = "no nome";
+//	//strcpy(s, "no");
+//	return 0;
+//}
+
