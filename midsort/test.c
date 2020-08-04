0d0b42a7051aead43a132b7a35f37859580b40a8
WeiMengXS <1254688559@qq.com>
2020-08-04 16:31:23 +0800

    _midsort

---
+++ midsort/test.c
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    void reOrderArray(vector<int> &array) {
+              int k=0;
+                      for(int i=0;i<array.size();i++)
+                      {
+                                    
+                        if(array[i]&1){
+                                          int tmp=array[i];
+                                                          int j=i;
+                                                                          while(j>k)
+                                                                          {
+                                                                                                array[j]=array[j-1];
+                                                                                                                    j--;
+                                                                                                                                    
+                                                                          }
+                                                                                          array[k++]=tmp;
+                                                                                                      
+                        }
+                                
+                      }
+                          
+    }
+
+};
