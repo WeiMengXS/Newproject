4bb68a82c2b543c47993ed0a5637e5e9ed475a06
WeiMengXS <1254688559@qq.com>
2020-08-15 17:53:46 +0800

    sln

---
+++ 7-26/7-26/testcpp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string  s2("world");
+	string s1("hellow world");
+	
+	return 0;
+}
