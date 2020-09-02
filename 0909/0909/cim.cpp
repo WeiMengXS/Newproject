ee21933e86c8529885e87b257b25f33fc3ef15da
WeiMengXS <1254688559@qq.com>
2020-09-03 00:48:59 +0800

    tlog

---
+++ 0909/0909/cim.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<typeinfo>
+#include<map>
+using namespace std;
+int main()
+{
+	map<int, int> m;
+	m.insert(1,1);
+
+	//for (auto e : m)
+	//{
+	//	////cout << typeid(e).name() << endl;
+	//}
+}
